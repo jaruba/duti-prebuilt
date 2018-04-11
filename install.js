b2e7eb0d5edd7eb855e7a069e85e19267c207ac9
Alexandru Branza <branza.alex@gmail.com>
2018-04-11 19:33:03 +0300

    Use `utid` Instead of `duti` (better maintained)

--- install.js
+++ install.js
@@ -3,9 +3,9 @@ var fs = require('fs')
 var path = require('path');
 
 var user = 'jaruba'
-var tag = '0.0.1'
+var tag = '0.0.2'
 var repoName = 'duti-prebuilt'
-var pkg = 'duti'
+var pkg = 'utid'
 var url = 'https://github.com/' + user + '/' + repoName + '/releases/download/' + tag + '/' + pkg
 var binDir = path.resolve(__dirname, '../..', 'bin');
 
