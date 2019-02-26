4db6c24c97d15216e01f2e75b41dec8d5c9af12a
Jaruba Firanelli <branza.alex@gmail.com>
2019-02-26 09:27:33 +0200

    Install only on Darwin

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ var child = require('child_process')
 
 var back = '../..';
 var binDir = path.resolve(__dirname, back, 'bin');
+var duti
 
-// in case of asar, go one level down
-while (binDir.includes('/app.asar/') || binDir.includes('\\app.asar\\')) {
-    back += '/..'
-    binDir = path.resolve(__dirname, back, 'bin');
+if (process.platform == 'darwin') {
+
+	// in case of asar, go one level down
+	while (binDir.includes('/app.asar/') || binDir.includes('\\app.asar\\')) {
+	    back += '/..'
+	    binDir = path.resolve(__dirname, back, 'bin');
+	}
+
+	// for safety
+	if (!fs.existsSync(binDir)) {
+	  back += '/..'
+	  binDir = path.resolve(__dirname, back, 'bin');
+	}
+
+	duti = path.join(binDir, 'duti')
+
+	if (fs.existsSync(path.join(duti, 'duti')))
+	  duti = path.join(duti, 'duti')
+
 }
-
-// for safety
-if (!fs.existsSync(binDir)) {
-  back += '/..'
-  binDir = path.resolve(__dirname, back, 'bin');
-}
-
-var duti = path.join(binDir, 'duti')
-
-if (fs.existsSync(path.join(duti, 'duti')))
-  duti = path.join(duti, 'duti')
 
 module.exports = function(id, ext, type) {
   child.exec('"' + duti + '" -s ' + id + ' ' + ext + (type ? ' ' + type : ''));
--- install.js
+++ install.js
@@ -1,31 +1,34 @@
-var request = require('request')
-var fs = require('fs')
-var path = require('path');
 
-var user = 'jaruba'
-var tag = '0.0.2'
-var repoName = 'duti-prebuilt'
-var pkg = 'utid'
-var url = 'https://github.com/' + user + '/' + repoName + '/releases/download/' + tag + '/' + pkg
-var binDir = path.resolve(__dirname, '../..', 'bin');
+if (process.platform == 'darwin') {
+  var request = require('request')
+  var fs = require('fs')
+  var path = require('path');
 
-if (!fs.existsSync(binDir))
-  fs.mkdirSync(binDir);
+  var user = 'jaruba'
+  var tag = '0.0.2'
+  var repoName = 'duti-prebuilt'
+  var pkg = 'utid'
+  var url = 'https://github.com/' + user + '/' + repoName + '/releases/download/' + tag + '/' + pkg
+  var binDir = path.resolve(__dirname, '../..', 'bin');
 
-request
-  .get(url)
-  .on('error', function (err) {
-    throw err
-  })
-  .pipe(fs.createWriteStream(pkg))
-  .on('close', function () {
+  if (!fs.existsSync(binDir))
+    fs.mkdirSync(binDir);
 
-    if (!fs.existsSync(path.join(binDir, 'duti')))
-      fs.mkdirSync(path.join(binDir, 'duti'));
-
-    var newPath = path.join(binDir, 'duti', 'duti')
-
-    fs.rename(pkg, newPath, function() {
-      fs.chmod(newPath, '755')
+  request
+    .get(url)
+    .on('error', function (err) {
+      throw err
     })
-  })
+    .pipe(fs.createWriteStream(pkg))
+    .on('close', function () {
+
+      if (!fs.existsSync(path.join(binDir, 'duti')))
+        fs.mkdirSync(path.join(binDir, 'duti'));
+
+      var newPath = path.join(binDir, 'duti', 'duti')
+
+      fs.rename(pkg, newPath, function() {
+        fs.chmod(newPath, '755')
+      })
+    })
+}
